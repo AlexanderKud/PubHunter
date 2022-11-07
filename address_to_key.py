54b01a0ee8025496902df927459c2607bb0c48d1
Sezgin YILDIRIM <sezginyildirim@outlook.com.tr>
2022-11-07 15:31:06 +0300

    Update address_to_key.py

--- address_to_key.py
+++ address_to_key.py
@@ -1,55 +1,60 @@
 # 02/11/2022
 # Created by Sezgin YILDIRIM
 
-import multiprocessing as mp
+"""
+USEGE :
+> python -a 15JhYXn6Mx3oF4Y7PcTAv2wVVAuCFFQNiP -b 25
+> python -a 15JhYXn6Mx3oF4Y7PcTAv2wVVAuCFFQNiP -b 25 -r 2048 -c 6
+"""
+
+from argparse import ArgumentParser
+import multiprocessing as MP
 from random import randint
+import sys
 from secp256k1 import (
     b58_decode as b58,
     privatekey_to_h160 as p2h,
     privatekey_loop_h160_sse as loop
     )
 
-##############################################
-address = '13zb1hQbWVsc2S7ZTZnP2G4undNNpdh5so'
-
-bit = 66            # Range
-RANGE = 16384       # Stride in every range
-cpu_count = 4       # CPU COUNT
-compressed = True   # 'compressed = True' or 'uncompressed = False'
-##############################################
-
+parser = ArgumentParser(description='I am trying to access private key from given public key.', epilog='https://github.com/geokomplo/')
+parser.add_argument("-a", "--address", help = "P2PKH Address", required=True)
+parser.add_argument("-b", "--bit", help = "Bit Range (20 - 256)", required=True)
+parser.add_argument("-r", "--range", help = "The number or range of steps to search for in the fragmented bit range. | Default : 16384")
+parser.add_argument("-c", "--cpu", help = "CPU Count | Default : 4")
+if len(sys.argv)==1: parser.print_help(), sys.exit(1)
+args = parser.parse_args()
+address, bit, RANGE, cpu_count = args.address, int(args.bit), int(args.range) if args.range else 16384, int(args.cpu) if args.cpu else 4
 _ = 2**(bit-1)
 P = bytes.fromhex(b58(address)[2:-8])
 
 def found(x):
     for i in range(RANGE):
         _p, p_ = x + i, x - i
-        T = p2h(0, compressed, _p) + p2h(0, compressed, p_)
+        T = p2h(0, True, _p) + p2h(0, True, p_)
         if P in T:
-            V = hex(_p)[2:] if p2h(0, compressed, _p) == P else hex(p_)
+            V = hex(_p)[2:] if p2h(0, True, _p) == P else hex(p_)
             print('#'*30 + f'\nPrivate Key : {V}\n' + '#'*30)
             open('found.txt', 'a').write('#'*30 + f'\nPrivate Key : {V}\n' + '#'*30 + '\n')
-            foundit.set()
             return True
 
-def RUN():
-    while not quit.is_set():
+def RUN(q, f):
+    while not q.is_set():
         M = randint(16, 64)
         B = _ // M
         S = [_ + (B * i) for i in range(M + 1)]
         R = randint(1, S[1] - S[0])
         for I in range(M):
             K = S[I] + R
-            T = loop(RANGE, 0, compressed, K) + loop(RANGE, 0, compressed, K - RANGE)
+            T = loop(RANGE, 0, True, K) + loop(RANGE, 0, True, K - RANGE)
             if P in T:
-                found(K)
+                found(K), f.set()
                 break
 
 if __name__ == '__main__':
-    quit = mp.Event()
-    foundit = mp.Event()
+    q, f = MP.Event(), MP.Event()
     for i in range(cpu_count):
-        pc = mp.Process(target=RUN, args=( ))
-        pc.start()
-    foundit.wait()
-    quit.set()
+        PC = MP.Process(target=RUN, args=(q, f, ))
+        PC.start()
+    f.wait()
+    q.set()
