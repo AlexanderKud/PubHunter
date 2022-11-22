7b55b3193112f2fc003497642fe6f0caf6605e08
Sezgin YILDIRIM <sezginyildirim@outlook.com.tr>
2022-11-23 02:46:07 +0300

    Update subPuplic.py

--- subPuplic.py
+++ subPuplic.py
@@ -18,52 +18,56 @@ def bloom(N):
 	bits, hashes, bf = BL(BELOW, 0.000001)
 	return bits, hashes, bf, BELOW
 
-def above(bit, bitRange):
-	ABOVE = []
-	for I in range(bitRange-5):
-		bit = bit // 2
-		ABOVE.append(bit)
-	return ABOVE
-
-def sub(P, bitRange, FIRST):
-	KeysP, KeysI, bit, FIRST, N = [], [], 2**(bitRange-2), SM(FIRST), 2**10
-	for I in range(N):
-		R = randint(1, bit)
+def sub(P, bitRange):
+	KeysP, KeysI, bit, N = [], [], 2**(bitRange-3), 2**10
+	bit //= N
+	for I in range(1, N):
+		R = bit * I
 		A = SM(R)
-		AR = PA(P, A)
-		RA = PS(P, A)
-		KeysP.append(PS(AR, FIRST))
-		KeysP.append(PS(RA, FIRST))
+		KeysP.append(PA(P, A))
+		KeysP.append(PS(P, A))
 		KeysI.append(R)
 	return KeysP, KeysI
 
 class Multi:
-	def __init__(self, Q, F, bits, hashes, bf, BELOW, ABOVE, N, P, KeysP, KeysI, FIRST):
+	def __init__(self, Q, F, bits, hashes, bf, BELOW, N, P, KeysP, KeysI, bit):
 		self.Q, self.F, self.bits, self.hashes, self.bf, self.BELOW, \
-		self.ABOVE, self.N, self.P, self.KeysP, self.KeysI, self.FIRST = \
-		Q, F, bits, hashes, bf, BELOW, ABOVE, N, P, KeysP, KeysI, FIRST
+		self.N, self.P, self.KeysP, self.KeysI, self.bit = \
+		Q, F, bits, hashes, bf, BELOW, N, P, KeysP, KeysI, bit
 		self.main()
 
 	def found(self, RIP, CC):
 		if RIP in self.BELOW:
 			print('JOINJOINJOIN')
+			self.F.set()
 			for I in range(1, self.N):
 				if SM(I)[1:33] == RIP:
 					break
 
 			for R in self.KeysI:
-				SUM = sum(CC) + self.FIRST + I
+				SUM = sum(CC) + I
 				TETRAGRAMMATON = [abs(SUM + R), abs(SUM - R), abs(SUM + R - I*2), abs(SUM - R - I*2)]
 				for YHVH in TETRAGRAMMATON:
 					if SM(YHVH) == self.P:
 						F = f'\n{"*"*80}\nPrivate KEY : 0x{hex(YHVH)[2:].zfill(64).upper()}\
 							\nPublic KEY  : {p2c(SM(YHVH)).upper()}\n{"*"*80}'
 						print(F), open('found.txt', 'a').write(F)
-						self.F.set(), sys.exit()
+						sys.exit()
+
+	def paradigm(self):
+		S, B, P = [], 2**(self.bit), randint(2**(self.bit-1), 2**self.bit)
+		for A in range(self.bit-5):
+			B //= 2
+			P -= B
+			if P <= 0:
+				P += B
+			else:
+				S.append(B)
+		return S
 
 	def main(self):
 		while not self.Q.is_set():
-			AC = [A for A in self.ABOVE if randint(0, 1) == 1]
+			AC = self.paradigm()
 			for i in AC:
 				print(hex(i))
 			for RIP in self.KeysP:
@@ -76,20 +80,16 @@ class Multi:
 						self.found(RIP[1:33], CC)
 						
 if __name__ == '__main__':
-	public_key = '02ceb6cbbcdbdf5ef7150682150f4ce2c6f4807b349827dcdbdd1f2efa885a2630'
-	bitRange = 120
+	public_key = '03f46f41027bbf44fafd6b059091b900dad41e6845b2241dc3254c7cdd3c5a16c6'
+	bitRange = 50
 	N = 5000000
-	bit = 2**bitRange
 	P = p2u(public_key)
-	ABOVE = above(bit, bitRange)
-	FIRST = ABOVE[0]
-	KeysP, KeysI = sub(P, bitRange, FIRST)
+	KeysP, KeysI = sub(P, bitRange)
 	bits, hashes, bf, BELOW = bloom(N)
-	ABOVE.pop(0)
 	#######################
 	Q, F = MP.Event(), MP.Event()
 	for j in range(4):
-		PC = MP.Process(target=Multi, args=(Q, F, bits, hashes, bf, BELOW, ABOVE, N, P, KeysP, KeysI, FIRST))
+		PC = MP.Process(target=Multi, args=(Q, F, bits, hashes, bf, BELOW, N, P, KeysP, KeysI, bitRange))
 		PC.start()
 	F.wait()
 	Q.set()
